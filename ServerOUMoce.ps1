64b85f7504549d291d7f31e8b271df5fcba61956
Mad Coal <38477991+madcoal@users.noreply.github.com>
2018-04-17 19:12:31 -0500

    Update and rename Servers-DomainServers.ps1 to ServerOUMoce.ps1
    This Powershell Script moves a server objects in AD to the correct OU.

--- ServerOUMoce.ps1
+++ ServerOUMoce.ps1
@@ -1,2 +1,2 @@
-﻿### This will move servers form the Computer OU into Domain Servers OU ####
+﻿### This will move Server Objects to the correct OU ####
 Get-ADComputer -Filter { OperatingSystem -Like "*Server*" } -SearchBase "CN=*SourceOU*,DC=*Domain*,DC=local" | ForEach-Object { Move-ADobject -Identity $_ -TargetPath "OU=*DestinationOU*,DC=*Domain*,DC=local" }
